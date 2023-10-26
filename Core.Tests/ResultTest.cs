2bd93c3b1b767e653063f535d6d3772e8cdaeac2
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-10-26 18:50:52 +0200

    + 'Unwrap()' & 'UnwrapErr()'

--- Core.Tests/ResultTest.cs
+++ Core.Tests/ResultTest.cs
@@ -40,4 +40,46 @@ public class ResultTest
             return "";
         }, s => { return s; }));
     }
+
+    [Fact]
+    public void Unwrap_ReturnsOkResult()
+    {
+        const string expectedString = "abcd";
+
+
+        Result<string, int> result = Result<string, int>.Ok(expectedString);
+
+
+        Assert.Equal(expectedString, result.Unwrap());
+    }
+
+    [Fact]
+    public void Unwrap_ErrResult_ThrowsException()
+    {
+        Result<string, int> result = Result<string, int>.Err(1);
+
+
+        Assert.Throws<InvalidOperationException>(result.Unwrap);
+    }
+
+    [Fact]
+    public void UnwrapErr_ReturnsErrResult()
+    {
+        const string expectedString = "abcd";
+
+
+        Result<int, string> result = Result<int, string>.Err(expectedString);
+
+
+        Assert.Equal(expectedString, result.UnwrapErr());
+    }
+
+    [Fact]
+    public void UnwrapErr_OkResult_ThrowsException()
+    {
+        Result<int, string> result = Result<int, string>.Ok(1);
+
+
+        Assert.Throws<InvalidOperationException>(result.UnwrapErr);
+    }
 }
--- Core/Result.cs
+++ Core/Result.cs
@@ -19,6 +19,20 @@ public class Result<TOk, TError>
         return IsOk ? success(_ok) : failure(_error);
     }
 
+    public TOk Unwrap()
+    {
+        if (!IsOk) throw new InvalidOperationException("tried to unwrap 'Ok' when the result was an 'Err'.");
+
+        return _ok;
+    }
+
+    public TError UnwrapErr()
+    {
+        if (IsOk) throw new InvalidOperationException("tired to unwrap 'Err' when the result was an 'Ok'.");
+
+        return _error;
+    }
+
     public static Result<TOk, TError> Ok(TOk ok)
     {
         return new Result<TOk, TError>(ok, default, true);
