ff9734b29cdc6ca50d654f01864af8c5e19c8955
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-10-26 18:33:13 +0200

    + equality comparison

--- Core/Error.cs
+++ Core/Error.cs
@@ -4,4 +4,12 @@ public class Error<TErrorKind>(TErrorKind errorKind, string message) where TErro
 {
     public TErrorKind ErrorKind { get; } = errorKind;
     public string Message { get; } = message;
+
+    public override bool Equals(object? obj)
+    {
+        Error<TErrorKind> other = obj as Error<TErrorKind>;
+        if (other is null) return false;
+
+        return ErrorKind.Equals(other.ErrorKind) && Message.Equals(other.Message);
+    }
 }
