867a311f632dd832a097492679db5fd50ef4be6d
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-12-04 12:13:14 +0100

    + authorization bearer token

--- Core/HttpClientWrapper.cs
+++ Core/HttpClientWrapper.cs
@@ -1,16 +1,23 @@
+using System.Net.Http.Headers;
+
 namespace Core;
 
 public class HttpClientWrapper : IHttpClient
 {
     private readonly HttpClient _httpClient = new();
 
-    public Task<HttpResponseMessage> GetAsync(string? url)
+    public string AuthorizationBearerToken
     {
-        return _httpClient.GetAsync(url);
+        set => _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", value);
     }
 
     public void Dispose()
     {
         _httpClient.Dispose();
+    }
+
+    public Task<HttpResponseMessage> GetAsync(string? url)
+    {
+        return _httpClient.GetAsync(url);
     }
 }
