882c96f8b6bd2e96c566ec2100c8ca828cbe71fd
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2024-01-07 06:17:18 +0100

    + `ConfigClient` first revision

--- Core.Tests/Config/ConfigClientTest.cs
+++ Core.Tests/Config/ConfigClientTest.cs
@@ -7,39 +7,91 @@ namespace Cuplan.Core.Tests.Config;
 
 public class ConfigClientTest : TestBase
 {
-    [Fact]
-    public async Task Get_WorkingPathEmpty_DownloadsConfiguration()
-    {
-        const string expectedValue = "1234abcd";
-        const string host = "https://simpleg.eu";
-        const string stage = "dummy";
-        const string environment = "development";
-        const string component = "dummy";
-        const string filePath = "application.yaml";
-        const string configKey = "Parent:Child";
-        byte[] packageData = [];
-        Mock<IConfigDownloader> downloaderMock = new(MockBehavior.Strict);
-        downloaderMock.Setup(d => d.Download(
-            It.Is<string>(h => h == host),
-            It.Is<string>(s => s == stage),
-            It.Is<string>(e => e == environment),
-            It.Is<string>(c => c == component)))
-            .Returns(Task.FromResult(Result<byte[], Error>.Ok(packageData)));
-        Mock<IExtractor> extractorMock = new(MockBehavior.Strict);
-        extractorMock.Setup(e => e.Extract(It.Is<byte[]>(p => p.Equals(packageData)), It.IsAny<string>()))
-            .Returns(Result<Empty, Error>.Ok(new Empty()));
-        Mock<IConfigProvider> configProviderMock = new(MockBehavior.Strict);
-        configProviderMock.Setup(c =>
-                c.Get<string>(It.Is<string>(f => f == filePath), It.Is<string>(k => k == configKey)))
-            .ReturnsAsync(Result<string, Error>.Ok(expectedValue));
-        ConfigClient client = new(host, stage, environment, component,
-            downloaderMock.Object, extractorMock.Object, configProviderMock.Object);
+    private const string ExpectedValue = "1234abcd";
+    private const string Host = "https://simpleg.eu";
+    private const string Stage = "dummy";
+    private const string Environment = "development";
+    private const string Component = "dummy";
+    private const double DownloadAgainAfterSeconds = 1;
+    private const string FilePath = "application.yaml";
+    private const string ConfigKey = "Parent:Child";
 
-        Result<string, Error> value = await client.Get<string>(filePath, configKey);
+    private readonly string _workingPath = Guid.NewGuid().ToString();
+    private readonly byte[] _packageData = [];
+    private readonly Mock<IConfigDownloader> _downloaderMock;
+    private readonly Mock<IExtractor> _extractorMock;
+    private readonly Mock<IConfigProvider> _providerMock;
+    private readonly ConfigClient _client;
+
+    public ConfigClientTest()
+    {
+        _downloaderMock = new(MockBehavior.Strict);
+        _downloaderMock.Setup(d => d.Download(
+                It.Is<string>(h => h == Host),
+                It.Is<string>(s => s == Stage),
+                It.Is<string>(e => e == Environment),
+                It.Is<string>(c => c == Component)))
+            .Returns(Task.FromResult(Result<byte[], Error>.Ok(_packageData)));
+        _extractorMock = new(MockBehavior.Strict);
+        _extractorMock.Setup(e => e.Extract(It.Is<byte[]>(p => p.Equals(_packageData)), It.IsAny<string>()))
+            .Returns(Result<Empty, Error>.Ok(new Empty()));
+        _providerMock = new(MockBehavior.Strict);
+        _providerMock.Setup(c =>
+                c.Get<string>(It.Is<string>(f => f == FilePath), It.Is<string>(k => k == ConfigKey)))
+            .ReturnsAsync(Result<string, Error>.Ok(ExpectedValue));
+        _providerMock.Setup(c => c.CleanCache());
+        _client = new(Host, Stage, Environment, Component, _workingPath, DownloadAgainAfterSeconds,
+            _downloaderMock.Object, _extractorMock.Object, _providerMock.Object);
+    }
+    
+    [Fact]
+    public async Task Get_EmptyWorkingPath_DownloadsConfiguration()
+    {
+        Result<string, Error> result = await _client.Get<string>(FilePath, ConfigKey);
         
-        client.Dispose();
+        _client.Dispose();
+        AssertExpectedValue(result);
+        AssertCompleteFlowExecutedTimes(1);
+    }
+
+    [Fact]
+    public async Task Get_AfterDownloadAgain_Downloads()
+    {
+        await _client.Get<string>(FilePath, ConfigKey);
+        Thread.Sleep(1500);
+
+        Result<string, Error> result = await _client.Get<string>(FilePath, ConfigKey);
         
-        Assert.True(value.IsOk);
-        Assert.Equal(expectedValue, value.Unwrap());
+        _client.Dispose();
+        AssertExpectedValue(result);
+        AssertCompleteFlowExecutedTimes(2);
+    }
+
+    [Fact]
+    public void Dispose_RemovesWorkingPath()
+    {
+        Directory.CreateDirectory(_workingPath);
+        
+        _client.Dispose();
+        
+        Assert.False(Directory.Exists(_workingPath));
+    }
+
+    private void AssertExpectedValue(Result<string, Error> result)
+    {
+        Assert.True(result.IsOk, "Expected result to be ok in order to extract value.");
+        Assert.Equal(ExpectedValue, result.Unwrap());
+    }
+
+    private void AssertCompleteFlowExecutedTimes(int times)
+    {
+        _downloaderMock.Verify(d => d.Download(
+            It.Is<string>(h => h == Host),
+            It.Is<string>(s => s == Stage),
+            It.Is<string>(e => e == Environment),
+            It.Is<string>(c => c == Component)), Times.Exactly(times));
+        _extractorMock.Verify(e => e.Extract(It.Is<byte[]>(p => p.Equals(_packageData)), It.IsAny<string>()), Times.Exactly(times));
+        _providerMock.Verify(p => p.CleanCache(), Times.Exactly(times));
+        _providerMock.Verify(p => p.Get<string>(It.Is<string>(f => f == FilePath), It.Is<string>(k => k == ConfigKey)), Times.Exactly(times));
     }
 }
--- Core/Config/ConfigClient.cs
+++ Core/Config/ConfigClient.cs
@@ -6,17 +6,31 @@ public class ConfigClient : IDisposable
     private readonly string _stage;
     private readonly string _environment;
     private readonly string _component;
+    private readonly string _workingPath;
+    private readonly double _downloadAgainAfterSeconds;
     private readonly IConfigDownloader _downloader;
     private readonly IExtractor _extractor;
     private readonly IConfigProvider _provider;
     
-    public ConfigClient(string host, string stage, string environment, string component, IConfigDownloader downloader,
-        IExtractor extractor, IConfigProvider provider)
+    private DateTime _lastDownload = DateTime.UnixEpoch;
+    
+    public ConfigClient(
+        string host,
+        string stage,
+        string environment,
+        string component,
+        string workingPath,
+        double downloadAgainAfterSeconds,
+        IConfigDownloader downloader,
+        IExtractor extractor,
+        IConfigProvider provider)
     {
         _host = host;
         _stage = stage;
         _environment = environment;
         _component = component;
+        _workingPath = workingPath;
+        _downloadAgainAfterSeconds = downloadAgainAfterSeconds;
         _downloader = downloader;
         _extractor = extractor;
         _provider = provider;
@@ -24,10 +38,40 @@ public class ConfigClient : IDisposable
 
     public void Dispose()
     {
+        Directory.Delete(_workingPath, true);
     }
 
     public async Task<Result<T, Error>> Get<T>(string filePath, string key)
     {
+        TimeSpan timeSinceLastDownload = DateTime.Now - _lastDownload;
+        
+        if (timeSinceLastDownload.TotalSeconds > _downloadAgainAfterSeconds ||
+            !Directory.Exists(_workingPath))
+        {
+            Result<Empty, Error> initResult = await InitializeConfigurationWithinWorkingPath();
+
+            if (!initResult.IsOk)
+            {
+                return Result<T, Error>.Err(initResult.UnwrapErr());
+            }
+        }
+        
         return await _provider.Get<T>(filePath, key);
+    }
+
+    private async Task<Result<Empty, Error>> InitializeConfigurationWithinWorkingPath()
+    {
+        Directory.CreateDirectory(_workingPath);
+
+        Result<byte[], Error> downloadResult = await _downloader.Download(_host, _stage, _environment, _component);
+
+        if (!downloadResult.IsOk)
+        {
+            return Result<Empty, Error>.Err(downloadResult.UnwrapErr());
+        }
+
+        _lastDownload = DateTime.Now;
+        _provider.CleanCache();
+        return _extractor.Extract(downloadResult.Unwrap(), _workingPath);
     }
 }
