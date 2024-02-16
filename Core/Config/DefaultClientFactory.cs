1f8a4ca4498aa5d0d1a907a7a05018090aa20254
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2024-02-16 19:21:47 +0100

    + missing `AccessToken` setting

--- Core/Config/DefaultClientFactory.cs
+++ Core/Config/DefaultClientFactory.cs
@@ -2,11 +2,15 @@ namespace Core.Config;
 
 public class DefaultClientFactory : IClientFactory
 {
-    public Result<Client, Error> Build(string host, string stage, string environment, string component,
+    public Result<Client, Error> Build(string accessToken, string host, string stage, string environment,
+        string component,
         string workingPath,
         int downloadTimeoutInMilliseconds)
     {
-        IDownloader downloader = new HttpDownloader(new HttpClientWrapper(), downloadTimeoutInMilliseconds);
+        HttpClientWrapper wrapper = new();
+        wrapper.AuthorizationBearerToken = accessToken;
+
+        IDownloader downloader = new HttpDownloader(wrapper, downloadTimeoutInMilliseconds);
         IExtractor extractor = new ZipExtractor();
         IGetter getter = new FileGetter(workingPath, new Cache(TimeSpan.FromHours(1)), TimeSpan.FromHours(1));
 
--- Core/Config/IClientFactory.cs
+++ Core/Config/IClientFactory.cs
@@ -5,13 +5,15 @@ public interface IClientFactory
     /// <summary>
     ///     Builds a <see cref="Client" /> instance.
     /// </summary>
+    /// <param name="accessToken">Access token to be used to access the configuration server.</param>
     /// <param name="host">The URL of the configuration server.</param>
     /// <param name="stage">Stage of the configuration to be retrieved.</param>
     /// <param name="environment">Environment of the configuration to be retrieved.</param>
     /// <param name="component">Component, aka microservice, whose configuration wants to be retrieved.</param>
     /// <param name="workingPath">Path where the configuration will be stored.</param>
-    /// <param name="downloadTimeoutInSeconds">Timeout download after specified milliseconds.</param>
+    /// <param name="downloadTimeoutInMilliseconds">Timeout download after specified milliseconds.</param>
     /// <returns><see cref="Client" /> if it is successful, or an <see cref="Error" /> otherwise.</returns>
-    public Result<Client, Error> Build(string host, string stage, string environment, string component,
+    public Result<Client, Error> Build(string accessToken, string host, string stage, string environment,
+        string component,
         string workingPath, int downloadTimeoutInMilliseconds);
 }
